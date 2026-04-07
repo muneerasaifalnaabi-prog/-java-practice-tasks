55883745ee99c30c11d4cc0472ffd955937d9f49
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-07 11:31:40 +0400

    Task 9:  maxMod5

---
+++ src/maxMod5.java
@@ -0,0 +1,16 @@
+public class maxMod5 {
+    public static void main(String[] args) {
+        System.out.println(maxMod5(2,3));
+        System.out.println(maxMod5(6, 2));
+
+    }
+    public static int maxMod5(int a, int b){
+        if(a==b){
+            return 0;
+        }
+        if (a%5==b%5){
+            return Math.min(a,b);
+        }
+        return Math.max(a,b);
+    }
+}
